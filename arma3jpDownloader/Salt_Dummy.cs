c5cf23c7768d6e9f167fd41237a7349df7766a77
jontamasan <levelsync@gmail.com>
2014-04-01 16:41:44 +0900

    ダミー Salt.cs 追加

---
+++ arma3jpDownloader/Salt_Dummy.cs
@@ -0,0 +1,14 @@
+﻿
+namespace arma3jpDownloader {
+    /// <summary>
+    /// パスワード暗号化用文字列
+    /// !!!コミットから除外すること!!!
+    /// </summary>
+    public class Salt {
+        public static string salt {
+            get {
+                return "8バイト以上の暗号鍵文字列";
+            }
+        }
+    }
+}
